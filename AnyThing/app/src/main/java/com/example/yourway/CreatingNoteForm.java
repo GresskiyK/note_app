3a722b4c9253f16ffddc94efe22be848c231a4a4
GresskiyK <kirillgressky@gmail.com>
2019-11-14 19:19:03 +0300

    add buttons to CreatingNoteForm

--- AnyThing/app/src/main/java/com/example/yourway/CreatingNoteForm.java
+++ AnyThing/app/src/main/java/com/example/yourway/CreatingNoteForm.java
@@ -2,7 +2,9 @@ package com.example.yourway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreatingNoteForm extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CreatingNoteForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creating_note_form);
+    }
+
+    public void back_to_menu(View view) {
+        Intent intent = new Intent(CreatingNoteForm.this, MenuActivity.class);
+        startActivity(intent);
     }
 }
